e5e84dd9ecb1a42d721de2ba7c2eef3653be8a70
Aditya Todi <adityatodi.surat@gmail.com>
2024-06-06 08:32:39 +0530

    Update News.js

--- src/components/News.js
+++ src/components/News.js
@@ -19,6 +19,7 @@ export class News extends Component {
 
   constructor(props) {
     super(props);
+    console.log(this.props.country);
     this.state = {
       articles: [],
       loading: true,
@@ -43,6 +44,25 @@ export class News extends Component {
     });
     this.props.setProgress(100);
   }
+
+  async componentDidUpdate(prevProps) {
+    if (prevProps.country !== this.props.country) {
+      this.props.setProgress(10);
+      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apikey}&page=1&pageSize=${this.props.pageSize}`;
+      this.setState({ loading: true });
+      this.props.setProgress(50);
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      this.props.setProgress(70);
+      this.setState({
+        articles: parsedData.articles,
+        totalResults: parsedData.totalResults,
+        loading: false,
+      });
+      this.props.setProgress(100);
+    }
+  }
+  
   fetchMoreData = async () => {
     this.setState({ page: this.state.page + 1 });
     const url = `https://newsapi.org/v2/top-headlines?country=${
